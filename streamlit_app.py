4b406d318076287734f19a7a17485b01942db6cd
Hugo <115502940+HugoSPN@users.noreply.github.com>
2022-10-11 16:05:51 +0200

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,6 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfru
 strealit.write('Thanks for adding ', add_my_fruit)
 
 #This will not work correctly, but just go with it for now
-my_cur_execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+my_cur_execute("insert into fruit_load_list values ('from streamlit')")
 
 
